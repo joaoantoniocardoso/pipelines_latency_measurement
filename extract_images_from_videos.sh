73504768d4fc211f9086ca3b1b2839b61b04aebc
João Antônio Cardoso <joao.maker@gmail.com>
2023-05-10 11:11:55 -0300

    Place extracted images into a subfolder

--- extract_images_from_videos.sh
+++ extract_images_from_videos.sh
@@ -1,4 +1,10 @@
-#!/usr/env bash
+#!/usr/bin/env bash
+
+mkdir -p extracted_images/rtsp_to_webrtc
+\rm -rf extracted_images/rtsp_to_webrtc/* ; ffmpeg -i rtsp_to_webrtc.mp4 -vf fps=60 extracted_images/rtsp_to_webrtc/img%06d.png
+
+mkdir -p extracted_images/rtsp_to_webrtc_better
+\rm -rf extracted_images/rtsp_to_webrtc_better/* ; ffmpeg -i rtsp_to_webrtc_better.mp4 -vf fps=60 extracted_images/rtsp_to_webrtc_better/img%06d.png
 
 mkdir -p extracted_images
 \rm -rf extracted_images/* ; ffmpeg -i rtsp_to_webrtc.mp4 -vf fps=60 extracted_images/img%06d.png
